905af816bac7241499ac21d75e4eadd3b56c5fb4
Strato75 <giovannibusonera@gmail.com>
2020-03-22 16:20:35 +0100

    Changes and new script to automate the update of figures

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+jupyter nbconvert --execute --to notebook --inplace global_world_stats.ipynb
 jupyter nbconvert --execute --to notebook --inplace whole_nation_stats.ipynb
 jupyter nbconvert --execute --to notebook --inplace per_region_stats.ipynb
 jupyter nbconvert --execute --to notebook --inplace per_province_choropleth_diagram.ipynb
