7eadbb7e815d14c17b1db12cbb741fd6187f3c57
Strato75 <giovannibusonera@gmail.com>
2020-03-26 23:49:51 +0100

    run_all script does not update notebooks content

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-jupyter nbconvert --execute --to notebook --inplace global_world_stats.ipynb
-jupyter nbconvert --execute --to notebook --inplace whole_nation_stats.ipynb
-jupyter nbconvert --execute --to notebook --inplace per_region_stats.ipynb
-jupyter nbconvert --execute --to notebook --inplace per_province_choropleth_diagram.ipynb
+#jupyter nbconvert --execute --to notebook --inplace global_world_stats.ipynb
+#jupyter nbconvert --execute --to notebook --inplace whole_nation_stats.ipynb
+#jupyter nbconvert --execute --to notebook --inplace per_region_stats.ipynb
+#jupyter nbconvert --execute --to notebook --inplace per_province_choropleth_diagram.ipynb
+#jupyter nbconvert --execute global_world_stats.ipynb
+jupyter nbconvert --execute whole_nation_stats.ipynb
+jupyter nbconvert --execute per_region_stats.ipynb
+jupyter nbconvert --execute per_province_choropleth_diagram.ipynb
