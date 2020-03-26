21fdfbd74fd92ef45fbe45e755de71a92b790b6d
Strato75 <giovannibusonera@gmail.com>
2020-03-27 00:38:48 +0100

    minor change

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -3,7 +3,7 @@
 #jupyter nbconvert --execute --to notebook --inplace whole_nation_stats.ipynb
 #jupyter nbconvert --execute --to notebook --inplace per_region_stats.ipynb
 #jupyter nbconvert --execute --to notebook --inplace per_province_choropleth_diagram.ipynb
-#jupyter nbconvert --execute global_world_stats.ipynb
+jupyter nbconvert --execute global_world_stats.ipynb
 jupyter nbconvert --execute whole_nation_stats.ipynb
 jupyter nbconvert --execute per_region_stats.ipynb
 jupyter nbconvert --execute per_province_choropleth_diagram.ipynb
