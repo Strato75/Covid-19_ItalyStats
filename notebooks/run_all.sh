4ac916e31a31195c9867cee7df4fc9eae0b4d767
Strato75 <giovannibusonera@gmail.com>
2020-03-28 18:35:13 +0100

    bugfixes for per region processing

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -6,4 +6,5 @@
 jupyter nbconvert --execute global_world_stats.ipynb
 jupyter nbconvert --execute whole_nation_stats.ipynb
 jupyter nbconvert --execute per_region_stats.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute Per_region_detailed_graphs.ipynb
 jupyter nbconvert --execute per_province_choropleth_diagram.ipynb
