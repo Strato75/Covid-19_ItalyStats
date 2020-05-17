99848751ce53d36464f7266dbc4d165d65d766d1
Strato75 <giovannibusonera@gmail.com>
2020-05-17 21:39:21 +0200

    notebooks updates

--- notebooks/run_all.sh
+++ notebooks/run_all.sh
@@ -3,8 +3,8 @@
 #jupyter nbconvert --execute --to notebook --inplace whole_nation_stats.ipynb
 #jupyter nbconvert --execute --to notebook --inplace per_region_stats.ipynb
 #jupyter nbconvert --execute --to notebook --inplace per_province_choropleth_diagram.ipynb
-jupyter nbconvert --execute global_world_stats.ipynb
-jupyter nbconvert --execute whole_nation_stats.ipynb
-jupyter nbconvert --execute per_region_stats.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute global_world_stats.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute whole_nation_stats.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute per_region_stats.ipynb
 jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute Per_region_detailed_graphs.ipynb
-jupyter nbconvert --execute per_province_choropleth_diagram.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute per_province_choropleth_diagram.ipynb
